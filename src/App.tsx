a8f4bb7887ddcaf90314a251e7af2959befa2bbb
leejimin <jimin980915@naver.com>
2025-08-24 15:13:37 +0900

    chore: 디자인 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, type JSX } from 'react'
 
-function App() {
+import reactLogo from '@/assets/react.svg'
+
+import '@/styles/App.css'
+
+function App(): JSX.Element {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
