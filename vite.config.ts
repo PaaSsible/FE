9800cb3b36a702e5a6191451be37a13983b309f6
leejimin <jimin980915@naver.com>
2025-08-30 02:14:48 +0900

    fix: vite base 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,10 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+  base: '/frontend/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
