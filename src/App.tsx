f84d8dc650b414921289fed213647374a850f8fb
leejimin <jimin980915@naver.com>
2025-09-01 11:19:46 +0900

    test: 도메인 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,9 @@ function App(): JSX.Element {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more this is tesssstr
+      </p>
     </>
   )
 }
