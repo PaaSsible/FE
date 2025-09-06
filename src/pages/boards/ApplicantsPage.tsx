24f38544533511a88f3e6c49b2e4196278279e92
seungju lee <dltmdwn0322@gmail.com>
2025-09-07 00:14:51 +0900

    feat: 라우팅 및 페이지 스켈레톤 추가

---
+++ src/pages/boards/ApplicantsPage.tsx
@@ -0,0 +1,5 @@
+import type { JSX } from 'react'
+
+export default function ApplicantsPage(): JSX.Element {
+  return <div>Applicants Page</div>
+}
---
+++ src/pages/projects/ProjectBoardPage.tsx
@@ -0,0 +1,5 @@
+import type { JSX } from 'react'
+
+export default function ProjectBoardPage(): JSX.Element {
+  return <div>This is Project Board Page</div>
+}
