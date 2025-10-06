77358dd73a026ada1b9b2df327678aeb00d72d0e
seungju lee <dltmdwn0322@gmail.com>
2025-10-06 19:23:37 +0900

    feat: Landing, Start, Boards 페이지 컴포넌트 구현
    - components/feature 하위에 각 페이지 섹션 단위로 컴포넌트 분리

---
+++ src/components/feature/landing/HeroSection.tsx
@@ -0,0 +1,21 @@
+import Button from '@/components/atoms/Button'
+import React, { useState } from 'react'
+import ScrollDown from './ScrollDown'
+import { InputField } from '../boards/InputField'
+import { Pagination } from '../boards/Pagination'
+
+interface HeroSectionProps {
+  onScrollDown: () => void
+}
+
+const HeroSection: React.FC<HeroSectionProps> = ({ onScrollDown }) => {
+  return (
+    <section className="bg-gray-0 relative flex h-screen flex-col items-center justify-center">
+      <h1 className="text-s1-bold text-gray-900">서비스 메인 카피 영역</h1>
+
+      <ScrollDown onClick={onScrollDown} />
+    </section>
+  )
+}
+
+export default HeroSection
---
+++ src/components/feature/landing/ScrollDown.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ArrowDownIcon from '@/assets/icons/arrow-down.svg'
+
+interface ScrollDownProps {
+  onClick: () => void
+}
+
+const ScrollDown: React.FC<ScrollDownProps> = ({ onClick }) => {
+  return (
+    <div
+      className="absolute bottom-20 left-1/2 flex h-[56px] w-[36px] -translate-x-1/2 animate-bounce cursor-pointer justify-center"
+      onClick={onClick}
+    >
+      <img src={ArrowDownIcon} alt="scroll down" className="h-[56px] w-[36px]" />
+    </div>
+  )
+}
+
+export default ScrollDown
