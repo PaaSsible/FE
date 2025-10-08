191fadb321e263d3dbf22c2bac1527d303e045ad
leejimin <jimin980915@naver.com>
2025-10-08 23:11:54 +0900

    feat: 인터셉터 도메인 분리, MSA 대응

---
+++ src/config/interceptors/user.interceptor.ts
@@ -0,0 +1,45 @@
+import axios, { AxiosError, type AxiosInstance } from 'axios'
+
+const API_URL = import.meta.env.VITE_API_USER_URL
+
+const PaaSsibleUser: AxiosInstance = axios.create({
+  baseURL: API_URL,
+  timeout: 30000,
+  withCredentials: true,
+})
+console.log(PaaSsibleUser.defaults)
+PaaSsibleUser.interceptors.request.use(
+  (config) => {
+    return config
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error)
+  },
+)
+
+PaaSsibleUser.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  async (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      try {
+        // refresh 요청 (서버가 refreshToken 쿠키 확인 후 accessToken 재발급)
+        await axios.post(`${API_URL}/auth/reissue`, {}, { withCredentials: true })
+
+        // 원래 요청 다시 시도
+        if (error.config) {
+          return PaaSsibleUser(error.config)
+        }
+      } catch (refreshError) {
+        // refresh도 실패 → 로그아웃 처리
+        console.error('Token refresh failed', refreshError)
+        await axios.post(`${API_URL}/auth/logout`, {}, { withCredentials: true })
+        // 로그인 페이지 이동
+      }
+    }
+    return Promise.reject(error)
+  },
+)
+
+export default PaaSsibleUser
