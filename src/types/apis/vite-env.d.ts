ac2ba998763062c0e719a14cb212086d6a3d8347
leejimin <jimin980915@naver.com>
2025-10-08 23:12:09 +0900

    feat: 인터셉터 도메인 분리, MSA 대응

--- src/types/apis/vite-env.d.ts
+++ src/types/apis/vite-env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_URL: string
+  readonly VITE_API_USER_URL: string
+  readonly VITE_API_BOARD_URL: string
+  readonly VITE_API_RECRUIT_URL: string
   readonly VITE_GOOGLE_CLIENT_ID: string
   readonly VITE_GOOGLE_CLIENT_SECRET: string
 }
