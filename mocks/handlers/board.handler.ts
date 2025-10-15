31354a6f87b1800fa1b116c464afca9ee6fb7ed9
leejimin <jimin980915@naver.com>
2025-10-16 00:52:22 +0900

    feat: Board 도메인 MSW 구현

--- mocks/handlers/board.handler.ts
+++ mocks/handlers/board.handler.ts
@@ -1,22 +1,17 @@
-import { http, HttpResponse } from 'msw'
+import { http, HttpHandler, HttpResponse } from 'msw'
 
-import * as boardAPISchemas from '@/types/apis/board/board.api.schemas'
 import * as BoardAPITypes from '@/types/apis/board/board.api.types'
 
-const API_URL = import.meta.env.VITE_API_BOARD_URL
+import { API_URL } from '.'
 
-export const boardHandlers = [
+const boardHandlers: HttpHandler[] = [
+  //보드 목록 조회
   http.get(`${API_URL}/boards`, ({ request }) => {
     // 쿼리 파라미터 가져오기
     const url = new URL(request.url)
     console.log('MSW handler called!', request.url)
     const status = url.searchParams.get('status') ?? undefined
     const keyword = url.searchParams.get('keyword') ?? undefined
-    // Zod로 런타임 검증
-    boardAPISchemas.getBoardListSchema.query.parse({
-      status,
-      keyword,
-    })
 
     // 전체 샘플 데이터
     const allBoards: BoardAPITypes.GetBoardList['Response']['data'] = [
@@ -54,9 +49,129 @@ export const boardHandlers = [
       errors: null,
     }
 
-    // Zod로 런타임 검증
-    boardAPISchemas.getBoardListSchema.response.parse(data)
+    return HttpResponse.json(data)
+  }),
+
+  //보드 생성
+  http.post(`/boards`, async ({ request }) => {
+    const body = (await request.clone().json()) as BoardAPITypes.PostBoard['Body']
+
+    const data: BoardAPITypes.PostBoard['Response'] = {
+      success: true,
+      message: '리소스가 생성되었습니다.',
+      data: null,
+      code: 'CREATED',
+      errors: null,
+    }
+
+    return HttpResponse.json(data)
+  }),
+
+  //보드 수정
+  http.put<{ boardId: string }>(`${API_URL}/boards/:boardId}`, async ({ params, request }) => {
+    const { boardId } = params
+    const body = (await request.clone().json()) as BoardAPITypes.PutBoard['Body']
+
+    const data: BoardAPITypes.PutBoard['Response'] = {
+      success: true,
+      message: '리소스가 수정되었습니다.',
+      data: null,
+      code: 'MODIFIED',
+      errors: null,
+    }
+
+    return HttpResponse.json(data)
+  }),
+
+  //보드 삭제
+  http.delete<{ boardId: string }>(`${API_URL}/boards/:boardId`, ({ params }) => {
+    const { boardId } = params
+
+    const data: BoardAPITypes.DeleteBoard['Response'] = {
+      success: true,
+      message: '리소스가 삭제되었습니다.',
+      data: null,
+      code: 'DELETED',
+      errors: null,
+    }
+
+    return HttpResponse.json(data)
+  }),
+
+  //보드 진입
+  http.get<{ boardId: string }>(`${API_URL}/boards/:boardId`, ({ params }) => {
+    const { boardId } = params
+
+    const data: BoardAPITypes.GetBoardDetail['Response'] = {
+      success: true,
+      message: '요청이 성공적으로 처리되었습니다.',
+      data: {
+        boardId: 2,
+        positionId: null,
+      },
+      code: 'OK',
+      errors: null,
+    }
+
+    return HttpResponse.json(data)
+  }),
+
+  //보드 멤버 조회
+  http.get<{ boardId: string }>(`${API_URL}/boards/:boardId/members`, ({ params }) => {
+    const { boardId } = params
+
+    const data: BoardAPITypes.GetBoardMember['Response'] = {
+      success: true,
+      message: '요청이 성공적으로 처리되었습니다.',
+      data: [
+        {
+          userId: 1,
+          userName: '박채은',
+          profileImageUrl: 'a.png',
+          role: 'OWNER',
+        },
+        {
+          userId: 2,
+          userName: 'chaeeun park',
+          profileImageUrl: 'b.png',
+          role: 'MEMBER',
+        },
+      ],
+      code: 'OK',
+      errors: null,
+    }
+
+    return HttpResponse.json(data)
+  }),
+  //보드 탈퇴
+  http.delete<{ boardId: string }>(`${API_URL}/boards/:boardId/members`, ({ params }) => {
+    const { boardId } = params
+
+    const data: BoardAPITypes.DeleteBoardMember['Response'] = {
+      success: true,
+      message: '리소스가 삭제되었습니다.',
+      data: null,
+      code: 'DELETED',
+      errors: null,
+    }
+
+    return HttpResponse.json(data)
+  }),
+  //보드 포지션 설정
+  http.patch<{ boardId: string }>(`${API_URL}/boards/:boardId/members`, ({ request, params }) => {
+    const { boardId } = params
+    const url = new URL(request.url)
+    const positionid = url.searchParams.get('positionId')
+
+    const data: BoardAPITypes.PatchBoardMember['Response'] = {
+      success: true,
+      message: '리소스가 수정되었습니다.',
+      data: null,
+      code: 'MODIFIED',
+      errors: null,
+    }
 
     return HttpResponse.json(data)
   }),
 ]
+export default boardHandlers
