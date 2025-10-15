16e9574cb7057de179cfda954be802e673b7a3ce
leejimin <jimin980915@naver.com>
2025-10-16 00:57:03 +0900

    fix: User 도메인 스키마 정수형 타입 수정

--- src/types/entities/user/user.schemas.ts
+++ src/types/entities/user/user.schemas.ts
@@ -1,19 +1,19 @@
 import z from 'zod'
 
 export const interestSchema = z.object({
-  id: z.bigint(),
+  id: z.number(),
   name: z.string(),
 })
 
 export const portfolioSchema = z.object({
-  id: z.bigint(),
+  id: z.number(),
   title: z.string().nullable(),
   description: z.string().nullable(),
   file: z.string().nullable(),
 })
 
 export const userSchema = z.object({
-  id: z.bigint(),
+  id: z.number(),
   nickname: z.string(),
   major: z.string().nullable(),
   profileImageUrl: z.url().nullable(),
