ca5b3b243cabf7a55fc2263cb21129a781593693
seungju lee <dltmdwn0322@gmail.com>
2025-10-18 15:11:23 +0900

    style: Modal UI 스타일 조정

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -30,12 +30,13 @@ const Modal: React.FC<ModalProps> = ({
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
       {/* 모달 본문 */}
       <div className="flex w-[484px] flex-col rounded-2xl bg-white px-8 pt-8 pb-[25px] text-start">
-        {/* 제목 */}
-        <div className="flex flex-col gap-4">
-          <h2 className="text-s1-bold text-start">{title}</h2>
-
-          {/* 설명 */}
-          {description && <div className="text-b5-medium text-gray-800">{description}</div>}
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-[15px]">
+            {/* 제목 */}
+            <h2 className="text-s1-bold text-start">{title}</h2>
+            {/* 설명 */}
+            {description && <div className="text-b5-medium text-gray-800">{description}</div>}
+          </div>
 
           {/* 버튼 영역 */}
           <div className="flex gap-4">
