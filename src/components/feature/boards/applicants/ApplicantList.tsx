def357a381eb7df6f662914f5129ba1777b9ac3b
seungju lee <dltmdwn0322@gmail.com>
2025-10-22 22:35:16 +0900

    feat: 지원자 관리 기본 UI

---
+++ src/components/feature/boards/applicants/ApplicantList.tsx
@@ -0,0 +1,38 @@
+// // interface ApplicantListProps {}
+
+// const mockBoards = [
+//   {
+//     id: 1,
+//     title: '팀 프로젝트 React 기반 프론트엔드 개발자 1명 구합니다',
+//     createdAt: '2시간 전',
+//     deadline: '2025-07-31',
+//     position: '프론트엔드',
+//     tags: ['React', '요구스택', '조건예시'],
+//     views: 133,
+//     applicants: 3,
+//   },
+//   {
+//     id: 2,
+//     title: '팀 프로젝트 React 기반 프론트엔드 개발자 1명 구합니다',
+//     createdAt: '2시간 전',
+//     deadline: '2025-07-31',
+//     position: '프론트엔드',
+//     tags: ['React', '요구스택', '조건예시'],
+//     views: 133,
+//     applicants: 3,
+//   },
+// ]
+
+// export default function ApplicantList() {
+//   return (
+//     <section>
+//       <p className="mb-4 font-medium text-gray-500">내 모집글 {mockBoards.length}건</p>
+
+//       <div className="flex flex-col gap-4">
+//         {mockBoards.map((board) => (
+//         //   <MyBoardItem key={board.id} {...board} onDeleteClick={onDeleteBoard} />
+//         ))}
+//       </div>
+//     </section>
+//   )
+// }
--- src/pages/boards/ApplicantsPage.tsx
+++ src/pages/boards/ApplicantsPage.tsx
@@ -1,5 +1,17 @@
-import type { JSX } from 'react'
+import BoardsPageHeader from '@/components/feature/boards/BoardsPageHeader'
+// import ApplicantList from '@/components/feature/boards/applicants/ApplicantList'
 
-export default function ApplicantsPage(): JSX.Element {
-  return <div>Applicants Page</div>
+export default function ApplicantsPage() {
+  const handleBackClick = () => {
+    window.history.back()
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col bg-gray-50 px-[144px] py-[44px] text-start">
+      {/* 상단 헤더 */}
+      <BoardsPageHeader title="지원자 확인" onBackClick={handleBackClick} />
+
+      <div className="mt-10">{/* <ApplicantList /> */}</div>
+    </div>
+  )
 }
