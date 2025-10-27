09c5980593fd8117b73f0fbd3b6ec56e4d75d4a4
leejimin <jimin980915@naver.com>
2025-10-27 22:34:35 +0900

    feat: chat 도메인 엔터티, API 스키마/타입 정의

--- src/types/entities/chat-room/chatRoom.schemas.ts
+++ src/types/entities/chat-room/chatRoom.schemas.ts
@@ -1,16 +1,24 @@
 import z from 'zod'
 
+import { userSchema } from '../user/user.schemas'
+
 export const chatRoomSchema = z.object({
-  id: z.bigint(),
-  name: z.string(),
-  isGroup: z.boolean(),
-  isDeleted: z.boolean(),
+  roomId: z.number(),
+  roomName: z.string(),
+  lastMessage: z.string(),
+  lastMessageTime: z.preprocess((val) => (typeof val === 'string' ? new Date(val) : val), z.date()),
+  unreadCount: z.number(),
 })
 
-export const messageTypeArray = ['TEXT', 'IMAGE', 'FILE'] as const
-export const messageSchema = z.object({
-  id: z.bigint(),
+export const chatMessageTypeArray = ['TEXT', 'IMAGE', 'FILE', 'SYSTEM'] as const
+export const chatMessageSchema = z.object({
+  id: z.number(),
+  roomId: chatRoomSchema.shape.roomId,
+  senderId: userSchema.shape.id,
+  senderName: userSchema.shape.nickname,
+  senderImage: userSchema.shape.profileImageUrl,
   content: z.string(),
-  fileUrl: z.url().nullable(),
-  type: z.enum(messageTypeArray),
+  type: z.enum(chatMessageTypeArray),
+  createAt: z.preprocess((val) => (typeof val === 'string' ? new Date(val) : val), z.date()),
+  readCount: z.number(),
 })
