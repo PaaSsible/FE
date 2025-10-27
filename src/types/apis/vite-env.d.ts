7fe549fa27bd9ae32f231aa00542f61c277ad4fc
leejimin <jimin980915@naver.com>
2025-10-27 22:43:29 +0900

    feat: chat 도메인 인터셉터 구현

--- src/types/apis/vite-env.d.ts
+++ src/types/apis/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_API_USER_URL: string
   readonly VITE_API_BOARD_URL: string
   readonly VITE_API_RECRUIT_URL: string
+  readonly VITE_API_CHAT_URL: string
   readonly VITE_GOOGLE_CLIENT_ID: string
   readonly VITE_GOOGLE_CLIENT_SECRET: string
   readonly VITE_REDIRECT_DEVELOP_URL: string
